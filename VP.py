1845d35bb3d6ddef6d4893f5f1810e6ddbe40bd4
Andrew Fedorov <35247871+ShotinthedarkK@users.noreply.github.com>
2018-01-10 16:28:32 +0300

    Почти хорошаяпопытка

--- VP.py
+++ VP.py
@@ -1,7 +1,7 @@
 # -*- coding: cp1251 -*-
 import csv
 import xlwt
-import numpy
+import numpy as np
 from tkinter import *
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
@@ -23,15 +23,15 @@ def open_dialog():# Выбор файла .csv для обработки
     reader = csv.DictReader(input_file, fieldnames=['Description', 'ТУ',
                                              'DocumentNumber', 'Quantity'])
     t_list = list()
-    t_dict = dict()
+    g_list = list()
     for rec in reader:
-        t_list.append ([rec['Description'], rec['ТУ']])
-        t_dict.update ({(rec['Description'], rec['ТУ'],
-                         rec['DocumentNumber']): rec['Quantity']})
+        t_list = [rec['Description'], rec['ТУ'],
+                   rec['DocumentNumber'], rec['Quantity']]
+        g_list.append(t_list)
     input_file.close()
-    return (t_dict)
+    return (g_list)
 
-def xls_config():# Настройка, создание файла xls
+def xls_write():# Настройка, создание файла xls
     font0 = xlwt.Font()
     font0.name = 'Times New Roman'
     font0.height = 320
@@ -45,8 +45,39 @@ def xls_config():# Настройка, создание файла xls
 
     wb = xlwt.Workbook()
     ws = wb.add_sheet('6Ц.270.000 ВП')
-    return
 
+    for i in range(len(fin)):
+        for j in range(len(fin[i])):
+            ws.write(i, j, fin[i] [j], style0)
+
+    wb.save('6Ц.270.000ВП.xls')
+    return 
+
+a = np.array(open_dialog())
+k = a.shape[0]
+while k > 0:
+    if a[k-1, 0] == a[k-2, 0]:
+        a [k-1, 0] = ''
+        a [k-1, 1] = ''
+    k = k- 1
+print (a)
+
+k = a.shape[0]
+i = 1
+fin = list()
+while i < k:
+    fin.append(list(a[i- 1]))
+    if (a[i- 1, 0] == '') & (a[i, 0] != '') & (a[i- 1, 2] != '6Ц.270.110 Э3'):
+        fin.append(['', '', '', int(a[i- 1, 3])+ int(a[i- 2, 3])])
+    elif (a[i- 1, 0] == '') & (a[i, 0] != '') & (a[i, 2] = '6Ц.270.110 Э3'):
+        fin.append(['', '', '', int(a[i- 1, 3])+ int(a[i- 2, 3])]) # некорректно
+    i = i+ 1
+a = fin
+print (a)
+
+xls_write()
+
+'''
 d_gen = open_dialog()
 d_repeat = dict() # Словарь с кол-во повторов
 comp_keys = list() # Все деспкрипшены
@@ -63,7 +94,7 @@ comp_keys.remove('Description')
 for i in comp_keys:
     d_repeat.update({i: t_keys.count(i)})  
     
-print (d_repeat)
+print (d_repeat)'''
 
 
 
